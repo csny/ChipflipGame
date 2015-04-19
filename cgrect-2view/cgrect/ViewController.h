7baa99ede914f6cc1d0a319941dec22fc727f959
csny <masaono_13@msn.com>
2015-04-20 00:53:23 +0900

    仮アップロード

---
+++ cgrect-2view/cgrect/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  cgrect
+//
+//  Created by macbook on 2014/09/10.
+//  Copyright (c) 2014年 macbook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import "AnimationView.h"
+
+@interface ViewController : UIViewController
+
+@property CALayer *layer;
+
+@end
